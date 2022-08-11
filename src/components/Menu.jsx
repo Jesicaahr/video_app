d2cc4cb27184e8b9db1c38b2b5d51039fd0d578d
jesicaahr <jesicaprilia18@gmail.com>
2022-08-12 00:16:45 +0700

    feat/side-bar

---
+++ src/components/Menu.jsx
@@ -0,0 +1,187 @@
+import React from 'react';
+import styled from 'styled-components';
+import LogoTube from '../img/logo.png';
+import {
+  Home,
+  ExploreOutlined,
+  SubscriptionsOutlined,
+  VideoLibraryOutlined,
+  HistoryOutlined,
+  LibraryMusicOutlined,
+  SportsBasketballOutlined,
+  SportsEsportsOutlined,
+  MovieCreationOutlined,
+  ArticleOutlined,
+  LiveTvOutlined,
+  SettingsOutlined,
+  FlagOutlined,
+  HelpOutlineOutlined,
+  SettingsBrightnessOutlined,
+  AccountCircleOutlined,
+} from '@mui/icons-material';
+
+const Container = styled.div`
+  flex: 1.4;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text};
+  height: 100vh;
+  font-size: 0.9rem;
+  position: sticky;
+  top: 0;
+  overflow-y: scroll;
+  //   border: 1px solid red;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
+
+const Wrapper = styled.div`
+  padding: 0.7rem 1.7rem;
+  margin-right: 0rem;
+  //   border: 1px solid yellow;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 1.2rem;
+`;
+
+const Img = styled.img`
+  height: 1.7rem;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  cursor: pointer;
+  //   border: 1px solid yellow;
+  padding: 0.5rem 0rem;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+const Button = styled.button`
+  padding: 5px 0.6rem;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 0.8rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
+
+const Menu = ({ darkMode, setDarkMode }) => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={LogoTube} />
+          YouyouTube
+        </Logo>
+        <Item>
+          <Home />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlined />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlined />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryOutlined />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlined />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlined /> SIGN IN
+          </Button>
+        </Login>
+        <Hr />
+        <Title>BEST OF YOUYOUTUBE</Title>
+        <Item>
+          <LibraryMusicOutlined />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlined />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlined />
+          Gaming
+        </Item>
+        <Item>
+          <MovieCreationOutlined />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlined />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlined />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlined />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlined />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlined />
+          Help
+        </Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
+          <SettingsBrightnessOutlined />
+          Light Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
